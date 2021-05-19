0241e3ced5dfa835073d1f9afb7b2f2eb6f371b3
tzvaita <tzvaita@gmail.com>
2021-05-19 23:28:06 +0200

    Add signing out

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import { SafeAreaView, ScrollView } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { AntDesign, SimpleLineIcons } from '@expo/vector-icons'
 import CustomListItem from '../components/CustomListItem'
 import { auth, db } from '../firebase';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
+  const [chats, setChats] = useState([])
+
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = db.collection('chats').onSnapshot(snapshot => (
+      setChats(snapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      }))
+    )
+    ))
+    return unsubscribe;
+  }, [])
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -13,22 +32,40 @@ const HomeScreen = ({navigation}) => {
       headerStyle: { backgroundColor: "#fff"},
       headerTitleStyle: {color: "black"},
       headerTintColor: "black",
-      headerLeft: () => {
+      headerLeft: () => (
       <View style={{ marginLeft: 20 }}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
           <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }}/>
         </TouchableOpacity>
-
-        
       </View>
-      },
+      ),
+      headerRight: () => (
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            width: 80,
+            marginRight: 20,
+          }}>
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name='camerao' size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('AddChat')}
+            activeOpacity={0.5}>
+            <SimpleLineIcons name='pencil' size={24} color='black' />
+          </TouchableOpacity>
+        </View>
+      )
     });
-  }, [])
+  }, [navigation])
 
   return (
     <SafeAreaView>
       <ScrollView>
-        <CustomListItem/>
+        {chats.map(({ id, data: { chatName }}) => (
+          <CustomListItem key={id} id={id} chatName={chatName} />
+        ))}
       </ScrollView>
     </SafeAreaView>
   )
